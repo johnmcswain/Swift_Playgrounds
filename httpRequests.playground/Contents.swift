e191a21a65c7e51c2650fc2f87851aa17f629dfe
John McSwain <johnmcswain@Excalibur.local>
2017-08-18 10:49:47 -0400

    Fixing the swift http request call.
    This was way to damned complicated

--- httpRequests.playground/Contents.swift
+++ httpRequests.playground/Contents.swift
@@ -1,37 +1,22 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import PlaygroundSupport
+
+URLCache.shared = URLCache(memoryCapacity: 0, diskCapacity: 0, diskPath: nil)
+
+PlaygroundPage.current.needsIndefiniteExecution = true
 
 
-var url = URL(string:"https://api.mockaroo.com/api/0a19de50?count=1000&key=bede6760")
-let urlRequest = URLRequest(url:url!)
-let config = URLSessionConfiguration.default
+var url = URL(string:"http://api.mockaroo.com/api/0a19de50?count=1000&key=bede6760")
 let session = URLSession.shared
 
-let task = session.dataTask(with: urlRequest){(data, response, error) in
-    
-    guard let responseData = data else {
-        print("Error: did not receive data")
-        return
+let task = session.dataTask(with: url!){ (data, _, _) -> Void in
+    if let data = data {
+        let string = String(data: data, encoding: String.Encoding.utf8)
+        print(string) //JSONSerialization
     }
-        do {
-            guard let item = try JSONSerialization.jsonObject(with: responseData, options: []) as? [String: AnyObject] else{
-                print("error trying to convert data to JSON")
-                return
-            }
-            for item in responseData{
-                
-                print(item)
-            }
-        } catch  {
-            print("error trying to convert data to JSON")
-            return
-        }
-        
-    
 }
 task.resume()
-
-
 
 
